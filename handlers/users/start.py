66d58499f62269e006e492a9bc84189de79e4384
Vlad Paulechka <vlad9.ru@gmail.com>
2022-02-25 15:29:34 +0300

    create new commands

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,9 +1,16 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
+from keyboards.default.start import start_defaultKB
 from loader import dp
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f'Привет, {message.from_user.full_name}!')
+    await message.answer(f'Привет, {message.from_user.full_name}!', reply_markup=await start_defaultKB())
+
+
+@dp.message_handler(text='Название бренда')
+async def name_brand(message: types.Message):
+    await message.answer(text="hello", reply_markup=await start_defaultKB())
+
