a9cace58a163e0c58541c6547780906cf9735bdd
Vlad Paulechka <vlad9.ru@gmail.com>
2022-02-27 20:00:10 +0300

    add id admins is file .env

--- data/config.py
+++ data/config.py
@@ -1,9 +1,10 @@
 import os
 from dotenv import load_dotenv
+from os import environ
 
 load_dotenv(".env")
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 
-admins = [
-	381252111,
-]
+
+async def load_admins() -> tuple:
+    return tuple(map(int, environ["ADMINS"].split(",")))
