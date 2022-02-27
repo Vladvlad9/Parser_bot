d46e90330531f070e8c54ff97b9fed9e606b0170
Vlad Paulechka <vlad9.ru@gmail.com>
2022-02-27 20:01:48 +0300

    create new file parcer_get_title.py

---
+++ handlers/users/parcer_get_title.py
@@ -0,0 +1,25 @@
+import json
+
+from aiogram import types
+from aiogram.dispatcher import FSMContext
+
+from loader import dp
+from states.wb_parcer_get_article import WBState
+from utils import WBParser
+
+
+@dp.message_handler(commands=['get_title'])
+async def name_brand(message: types.Message):
+    await message.answer(f"{message.from_user.full_name}, введи актикль")
+    await WBState.next()
+
+
+@dp.message_handler(state=WBState.name_article)
+async def load_name(message: types.Message, state: FSMContext):
+    await state.update_data(name_article=message.text)
+    article = await state.get_data()
+    result = await WBParser.get_response(str(article['name_article']))
+
+    await message.answer(f"Бренд: {str(result['brand_name'])}") if result else await message.answer(f"Товар не найден по введеному артиклю - {article}")
+
+    await state.finish()
